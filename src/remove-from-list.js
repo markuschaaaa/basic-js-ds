6de3eafc6077afa909cc4521a6c5d82de8f53514
markuschaaaa <chumakoff03@gmail.com>
2022-10-17 21:08:30 +0300

    feat: complete remove-from-array task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l) {
+    l.next = removeKFromList(l.next, k);
+    return (l.value === k) ? l.next : l;
+  }
+  return null;
 }
-
 module.exports = {
   removeKFromList
 };
